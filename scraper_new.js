437d32957d3cf97f93b6bbd66b68c94cc4e6cdc9
sanketsingh27 <singh.sanket27@gmail.com>
2024-04-20 20:52:18 +0530

    opening new page

--- scraper_new.js
+++ scraper_new.js
@@ -29,35 +29,66 @@ const scrapeProductData = async (productUrl = DEFAULT_URL) => {
 };
 
 async function getAmazonData(browser, title) {
-  console.log("Inside getAmazonData ", title);
   const page = await browser.newPage();
 
   const SESRCH_TERM = `foxtale ${title}`;
   const SEARCH_TERM_ARRAY = SESRCH_TERM.toLowerCase().split(" ");
+
   // Navigate to Amazon.in
   await page.goto(`https://www.amazon.in/s?k=${SESRCH_TERM}`);
 
   const searchResults = await page.$$('div[data-component-type="s-search-result"]');
 
-  console.log("Search Term ", SEARCH_TERM_ARRAY);
+  let productDetailPage;
   for (let index = 0; index < searchResults.length; index++) {
     const result = searchResults[index];
     const { title, dataIndex } = await page.evaluate((el) => {
       const spanElement = el.querySelector("h2 > a > span");
-      const title = spanElement ? spanElement.textContent.toLowerCase() : "";
-      const dataIndex = el.getAttribute("data-index");
+      const title = spanElement ? spanElement.textContent.toLowerCase() : ""; // tital to match
+      const dataIndex = el.getAttribute("data-index"); // to target proper item
       return { title, dataIndex };
     }, result);
 
     if (SEARCH_TERM_ARRAY.every((term) => title.includes(term))) {
+      const newPagePromise = getNewPageWhenLoaded(browser);
       const clickSelector = `div[data-index="${dataIndex}"]`;
-      console.log({ clickSelector });
-      await page.click(clickSelector);
+
+      page.click(clickSelector);
+      const newPage = await newPagePromise;
+
+      const pageTitle = await newPage.title();
+      console.log("title :  = = = ", pageTitle);
+
       break;
     }
+
+    // const { price, coupon } = await productPage(productDetailPage);
   }
 
   //   await page.close();
 }
+
+async function productPage(page) {
+  console.log("product detail page = ", page);
+  const pageTitle = await page.title();
+  console.log("product page titile = ", pageTitle);
+
+  //   scraping o
+}
+
+const getNewPageWhenLoaded = async (browser) => {
+  return new Promise((x) =>
+    browser.on("targetcreated", async (target) => {
+      if (target.type() === "page") {
+        const newPage = await target.page();
+        const newPagePromise = new Promise((y) =>
+          newPage.once("domcontentloaded", () => y(newPage))
+        );
+        const isPageLoaded = await newPage.evaluate(() => document.readyState);
+        return isPageLoaded.match("complete|interactive") ? x(newPage) : x(newPagePromise);
+      }
+    })
+  );
+};
 
 export default scrapeProductData;
